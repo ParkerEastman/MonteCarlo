3208efc1d146b9e3230720cd3d6d37eda8e3cea9
Parker Eastman <ParkerEastman@outlook.com>
2019-03-27 14:24:00 -0400

    Add project files.

---
+++ MonteCarlo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please specify the number of points");
+            int numOfPoints = Convert.ToInt32(Console.ReadLine());
+
+            XYcoord[] samplePoints = new XYcoord[numOfPoints];
+            Random pointSeed = new Random();
+
+            int insideCounter = 0;
+
+            for(int i = 0; i<numOfPoints; i++)
+            {              
+                samplePoints[i] = new XYcoord(pointSeed);
+
+                if (samplePoints[i].HypLength(samplePoints[i]) < 1)
+                {
+                    insideCounter++;
+                }
+
+            }
+
+            double piEstmation = 4 * ((1.0 * insideCounter) / (1.0 * numOfPoints));
+            double deviation = Math.Abs(piEstmation - Math.PI);
+            Console.WriteLine($"{insideCounter} points inside of the circle\n" +
+                $"Pi estimation: {piEstmation}\n" +
+                $"Deviation: {deviation}");
+            
+
+//	Print the value from step #4 along with the absolute value of the difference between your 
+//estimate of π and Math.Pi.
+
+        }
+    }
+}
---
+++ MonteCarlo/XYcoord.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+
+    public struct XYcoord
+    {
+        public double x;
+        public double y;
+
+        public XYcoord(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public XYcoord(Random item)
+        {
+            this.x = item.NextDouble();
+            this.y = item.NextDouble();
+        }
+
+        public double HypLength(XYcoord coords)
+            {
+            double hypotenuse = Math.Sqrt(Math.Pow(coords.x, 2) + Math.Pow(coords.y, 2));
+            return hypotenuse;
+            }
+
+    }
+
+
+
+
+
+}
+
+
